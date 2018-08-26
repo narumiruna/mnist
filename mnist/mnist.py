dc9ef8a778199ccc0ee9c5f3390dfe26ae2c1992
なるみ <weaper@gmail.com>
2018-08-26 16:32:56 +0800

    Build package

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -1,15 +1,15 @@
 import gzip
 import os
+import tempfile
+from urllib.request import urlretrieve
 
 import numpy as np
 
-from urllib.request import urlretrieve
 
-
-def to_one_hot(numbers, num_classes=10):
-    num_samples = len(numbers)
-    y = np.zeros(shape=(num_samples, num_classes))
-    y[np.arange(num_samples), numbers] = 1
+def to_one_hot(labels, num_classes=10):
+    num_labels = len(labels)
+    y = np.zeros(shape=(num_labels, num_classes))
+    y[np.arange(num_labels), labels] = 1
     return y
 
 
@@ -22,56 +22,53 @@ def load(path, offset, shape=None):
 
 
 class MNIST(object):
-    urls = ['http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
-            'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
-            'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
-            'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz']
+    urls = [
+        'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz'
+    ]
 
-    train_images_file = 'train-images-idx3-ubyte.gz'
-    train_labels_file = 'train-labels-idx1-ubyte.gz'
-    test_images_file = 't10k-images-idx3-ubyte.gz'
-    test_labels_file = 't10k-labels-idx1-ubyte.gz'
-
-    def __init__(self, root, shape=(-1, 1, 28, 28), one_hot=False):
-        self.root = root
+    def __init__(self, root=None, shape=(-1, 1, 28, 28), one_hot=False):
+        self.root = root if root else tempfile.TemporaryDirectory().name
         self.shape = shape
         self.one_hot = one_hot
 
-        if not self._check_exists():
-            print('Dataset not exists.')
-            self._download()
+        self._download()
 
-        self.train_images = load(os.path.join(self.root, self.train_images_file), offset=16, shape=shape)
-        self.train_labels = load(os.path.join(self.root, self.train_labels_file), offset=8)
-        self.test_images = load(os.path.join(self.root, self.test_images_file), offset=16, shape=shape)
-        self.test_labels = load(os.path.join(self.root, self.test_labels_file), offset=8)
+        train_images_file = os.path.join(self.root,
+                                         'train-images-idx3-ubyte.gz')
+        train_labels_file = os.path.join(self.root,
+                                         'train-labels-idx1-ubyte.gz')
+        test_images_file = os.path.join(self.root, 't10k-images-idx3-ubyte.gz')
+        test_labels_file = os.path.join(self.root, 't10k-labels-idx1-ubyte.gz')
+
+        self.train_images = load(train_images_file, offset=16, shape=shape)
+        self.train_labels = load(train_labels_file, offset=8)
+        self.test_images = load(test_images_file, offset=16, shape=shape)
+        self.test_labels = load(test_labels_file, offset=8)
 
         if one_hot:
             self.train_labels = to_one_hot(self.train_labels)
             self.test_labels = to_one_hot(self.test_labels)
 
     def _download(self):
-        os.makedirs(self.root, exist_ok=True)
+        if not os.path.exists(self.root):
+            os.makedirs(self.root)
         for url in self.urls:
-            print('Downloading {}'.format(url))
             filename = os.path.join(self.root, url.split('/')[-1])
             if not os.path.exists(filename):
+                print('Download {}'.format(url))
                 urlretrieve(url, filename=filename)
+            else:
+                print('{} exists'.format(filename))
 
-    def _check_exists(self):
-        return os.path.exists(os.path.join(self.root, self.train_images_file)) and \
-            os.path.exists(os.path.join(self.root, self.train_labels_file)) and \
-            os.path.exists(os.path.join(self.root, self.test_images_file)) and \
-            os.path.exists(os.path.join(self.root, self.test_labels_file))
+    @property
+    def num_train_samples(self):
+        assert len(self.train_images) == len(self.train_labels)
+        return len(self.train_images)
 
-
-def test():
-    mnist = MNIST('data', one_hot=True)
-    print(mnist.train_images.shape)
-    print(mnist.train_labels.shape)
-    print(mnist.test_images.shape)
-    print(mnist.test_labels.shape)
-
-
-if __name__ == '__main__':
-    test()
+    @property
+    def num_test_samples(self):
+        assert len(self.test_images) == len(self.test_labels)
+        return len(self.test_images)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MNIST",
+    version="0.1",
+    author="Narumi",
+    author_email="weaper@gmail.com",
+    packages=find_packages(),
+    install_requires=['numpy'])
